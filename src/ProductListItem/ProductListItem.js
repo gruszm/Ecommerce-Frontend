fd3059e8ad7cd23c7d3e9fb75f7e8491e3fb3267
gruszm <mariuszgruszczynski11@gmail.com>
2025-02-22 22:15:45 +0100

    Added a simple display of the list of products taken from backend

--- src/ProductListItem/ProductListItem.js
+++ src/ProductListItem/ProductListItem.js
@@ -1,5 +1,13 @@
 import "./ProductListItem.css";
 
 export default function ProductListItem(props) {
-
+    return (
+        <div className="products-list-item">
+            <p>id: {props.productData.id}</p>
+            <p>name: {props.productData.name}</p>
+            <p>price: {props.productData.price}</p>
+            <p>amount: {props.productData.amount}</p>
+            <p>categoryId: {props.productData.categoryId}</p>
+        </div>
+    );
 }
--- src/ProductsList/ProductsList.js
+++ src/ProductsList/ProductsList.js
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
 import "./ProductsList.css";
+import ProductsListItem from "../ProductListItem/ProductListItem";
 
 export default function ProductsList(props) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [productsList, setProductsList] = useState([]);
 
+    useEffect(() => {
+        const url = "http://" + process.env.REACT_APP_GATEWAY + ":" + process.env.REACT_APP_GATEWAY_PORT + "/api/public/products";
+
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setLoading(false);
+                setProductsList(res);
+            })
+            .catch(error => {
+                setLoading(false);
+                setError(true);
+                setErrorMessage(error.message);
+            });
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {errorMessage}</p>;
+
+    return (
+        <div>
+            {productsList.map(singleProduct => <ProductsListItem key={singleProduct.id} productData={singleProduct} />)}
+        </div>
+    );
 }
