677161132062a57a0f2294c9910217403e27d3af
gruszm <mariuszgruszczynski11@gmail.com>
2025-04-03 15:11:16 +0200

    Button for adding to cart

--- src/ProductListItem/ProductListItem.js
+++ src/ProductListItem/ProductListItem.js
@@ -2,12 +2,14 @@ import "./ProductListItem.css";
 
 export default function ProductListItem(props) {
     return (
-        <div className="products-list-item">
-            <p>id: {props.productData.id}</p>
-            <p>name: {props.productData.name}</p>
-            <p>price: {props.productData.price}</p>
-            <p>amount: {props.productData.amount}</p>
-            <p>categoryId: {props.productData.categoryId}</p>
+        <div className="product-list-item-container">
+            <div className="product-list-item-info">
+                <p>name: {props.productData.name}</p>
+                <p>price: {props.productData.price}</p>
+                <p>amount: {props.productData.amount}</p>
+                <p>categoryId: {props.productData.categoryId}</p>
+            </div>
+            <button className="add-to-cart">Dodaj do koszyka</button>
         </div>
     );
 }
