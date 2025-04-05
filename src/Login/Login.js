6913d9646650a0448618bcfdee306a08330fcacf
gruszm <mariuszgruszczynski11@gmail.com>
2025-04-05 17:25:11 +0200

    Debug variable added to show the result of login request

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,11 +1,47 @@
 import "./Login.css";
+import { useState } from "react";
 
 export default function Login(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [debug, setDebug] = useState("");
+
+    const login = async function (event) {
+        event.preventDefault();
+
+        const url = "http://" + process.env.REACT_APP_GATEWAY + ":" + process.env.REACT_APP_GATEWAY_PORT + "/api/login";
+        const body = { email: email, password: password };
+
+        const response = await fetch(url,
+            {
+                body: JSON.stringify(body),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+
+            setDebug(`${response.status}: ${errorResponse.message}`);
+        }
+        else {
+            const successResponse = await response.json();
+
+            setDebug(`Login successful, access token: ${successResponse.token}`);
+        }
+    };
+
     return (
-        <form action="" className="login-form-container">
-            <input type="text" name="login" id="login" placeholder="login" />
-            <input type="password" name="password" id="password" placeholder="hasło" />
-            <button type="submit">Zaloguj</button>
-        </form>
+        <div>
+            <form action="" className="login-form-container" onSubmit={login}>
+                <input type="email" name="email" id="email" placeholder="email" onChange={event => setEmail(event.target.value)} />
+                <input type="password" name="password" id="password" placeholder="hasło" onChange={event => setPassword(event.target.value)} />
+                <button type="submit">Zaloguj</button>
+            </form>
+            {debug}
+        </div>
     );
 }
